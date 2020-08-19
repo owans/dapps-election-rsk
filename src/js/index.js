b50b8b7953dc80797a6c1977358e6337ca4a9e5b
Owanate Amachree <amachreeowanate@gmail.com>
2020-08-19 20:09:33 +0100

    added contract and test files

--- src/js/index.js
+++ src/js/index.js
@@ -64,8 +64,33 @@ App = {
             // Render candidate Result
             var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
             candidatesResults.append(candidateTemplate);
+
+            // Render candidate ballot option
+        var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+        candidatesSelect.append(candidateOption);
           });
         }
+
+        return electionInstance.voters(App.account);
+        }).then(function(hasVoted) {
+          // Do not allow a user to vote
+        if(hasVoted) {
+      $('form').hide();
+    }
+
+
+    castVote: function() {
+      var candidateId = $('#candidatesSelect').val();
+      App.contracts.Election.deployed().then(function(instance) {
+        return instance.vote(candidateId, { from: App.account });
+      }).then(function(result) {
+        // Wait for votes to update
+        $("#content").hide();
+        $("#loader").show();
+      }).catch(function(err) {
+        console.error(err);
+      });
+    }
   
         loader.hide();
         content.show();
